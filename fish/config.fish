539a8ff8b33be5ce238b69d54f0128cd613b5745
dk <dk@DESKTOP-2LJD2F4>
2023-10-22 13:41:19 +0000

    add rye and juliaup

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,13 @@ fish_vi_key_bindings
 if test -d /opt/poetry/bin
     set -x PATH $PATH /opt/poetry/bin
 end
+
+# rye
+if test -d ~/.rye
+    set -x PATH $PATH ~/.rye/shims/rye
+end
+
+# juliaup
+if test -d ~/.juliaup
+    set -x PATH $PATH ~/.juliaup/bin/juliaup
+end
